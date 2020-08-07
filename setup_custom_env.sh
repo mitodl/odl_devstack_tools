604958b8a0f11f3d2ec22247e30e08807d3037d9
Gavin Sidebottom <gsidebo@mit.edu>
2020-08-07 17:35:02 -0400

    Support added PyPI dependencies

--- setup_custom_env.sh
+++ setup_custom_env.sh
@@ -3,18 +3,21 @@ set -e -o pipefail
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ADDED_REQUIREMENT_PREFIX="ADDED_REQ"
+ADDED_PYPI_REQUIREMENT_PREFIX="ADDED_PYPI_REQ"
 REPO_MOUNT_DIR="/edx/app/edxapp/venvs/edxapp/src"
 
 function echohighlight() {
   echo -e "\033[1;92m$@\e[0m"
 }
 
+# Apply changes to the relevant config files based on the patch files that were mounted into the container
 shopt -s nullglob
 for filepath in $SCRIPT_DIR/configpatch/*.json; do
   python $SCRIPT_DIR/updateconfig.py $filepath
 done
 
-# Loop through extra pip requirements specified by env variables
+# Loop through packages that were mounted to the file system in the container and should be installed via pip 
+# (These packages are specified by env variables with a certain prefix)
 repopath=
 env | awk -F = '$1 ~ /^'$ADDED_REQUIREMENT_PREFIX'_[a-zA-Z0-9_]*$/ { print $1 }' |
 while read var; do
@@ -40,4 +43,11 @@ while read var; do
     # This package uses setuptools. Install in editable mode.
     pip install -e $repopath --ignore-installed --no-deps
   fi
+done
+
+# Loop through extra pip dependencies and install them (also indicated by env variables with a certain prefix)
+env | awk -F = '$1 ~ /^'$ADDED_PYPI_REQUIREMENT_PREFIX'_[a-zA-Z0-9_]*$/ { print $1 }' |
+while read var; do
+  echohighlight "Installing external package: ${!var}"
+  pip install ${!var}
 done
